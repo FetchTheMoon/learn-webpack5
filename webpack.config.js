d55f7487c2d50db5e01ff25021642c57041012ac
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 14:22:13 +0800

    将package.json中的用来指定入口和输出目录的参数转移到webpack.config.js中作为配置项, 直接这样写会报错`The provided value "./dist/main.js" is not an absolute path!`

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'build.js',
+        path: "./dist/main.js" // The provided value "./dist/main.js" is not an absolute path!
+    }
+}
