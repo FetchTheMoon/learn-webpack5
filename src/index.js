52ec156f188ca2e1451098eb5496f21b9e3744e6
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 15:31:55 +0800

    为什么需要css-loader? 直接import css文件会报错: You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import {sum, square} from './js/utils.js'; // 通过手动引入, 必须加js后缀, 否则报错找不到文件
 const getInfo = require('./js/api.js'); // Uncaught ReferenceError: require is not defined
-import './js/lg'; // 将log.js加入依赖图(注意现在就没有加js后缀)
+// import './js/lg'; // 将log.js加入依赖图(注意现在就没有加js后缀)
+
+import './js/login';
 
 console.log(sum(10, 20));
 console.log(square(10));
---
+++ src/js/login.js
@@ -0,0 +1,11 @@
+import '../css/login.css'; // You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file.
+
+function login() {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = '拉勾教育前端';
+    h2.className = 'title';
+    return h2;
+}
+
+
+document.body.append(login());
