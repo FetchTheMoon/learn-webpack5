08b1be1e5e204700c415ec361001f9fcf469b747
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 13:54:05 +0800

    CommonJS 规范导出/导入, 直接使用会报错

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {sum, square} from './js/utils.js'; // 通过手动引入, 必须加js后缀, 否则报错找不到文件
-
+const getInfo = require('./js/api.js'); // Uncaught ReferenceError: require is not defined
 
 console.log(sum(10, 20));
 console.log(square(10));
+console.log(getInfo());
---
+++ src/js/api.js
@@ -0,0 +1,9 @@
+const getInfo = () => {
+    return {
+        name: 'fet',
+        age: 22
+    }
+}
+
+// CommonJS 规范导出
+module.exports = getInfo;
