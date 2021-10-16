350eb07e1fb5a10b3a363e1bf814d99889813e59
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 18:21:18 +0800

    解决src="[object Module]"的第一种办法

--- src/js/image.js
+++ src/js/image.js
@@ -4,7 +4,7 @@ function packImg() {
 
     const img = document.createElement("img");
 
-    img.src = require('../img/0.jpg');
+    img.src = require('../img/0.jpg').default; // 解决src="[object Module]"
     // You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders
     // (Source code omitted for this binary file)
 
