30acf445648aa84cb15aea0d0708afd8db6c19ef
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 14:46:13 +0800

    可以通过`webpack --config filename.js`来自定义配置文件名

--- lg.config.js
+++ lg.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'build.js',
+        filename: 'main.js',
         // path: "./dist/main.js" // The provided value "./dist/main.js" is not an absolute path!
         path: path.resolve(__dirname, 'dist'),
     }
