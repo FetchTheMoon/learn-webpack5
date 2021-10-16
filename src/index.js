a074500c3d33ea68763c95a39241ac9f2869e095
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 13:50:08 +0800

    ES6导出/导入/使用/引入

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {sum, square} from './js/utils.js'; // 通过手动引入, 必须加js后缀, 否则报错找不到文件
+
+
+console.log(sum(10, 20));
+console.log(square(10));
---
+++ src/js/utils.js
@@ -0,0 +1,10 @@
+const sum = (a, b) => {
+    return a + b;
+};
+
+const square = (m) => {
+    return m * m;
+};
+
+// ES6 导出
+export {sum, square}
