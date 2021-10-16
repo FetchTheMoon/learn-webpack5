8a690b4ce082692fce949b4f92ceaffe86e8711b
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 18:34:33 +0800

    CSS设置background-image 直接import导入, 会生成一个中间文件`dc47aa14ce851396fff0.jpg`, 其中的文本内容是指向图片文件`aa1f74...jpg`, 这个背景图片是显示不出来的

--- src/js/image.js
+++ src/js/image.js
@@ -1,4 +1,5 @@
 import imgSrc from '../img/0.jpg';
+import '../css/img.css';
 
 function packImg() {
 
@@ -8,12 +9,15 @@ function packImg() {
 
     // img.src = require('../img/0.jpg').default; // 解决src="[object Module]"
     // img.src = require('../img/0.jpg');
-    img.src = imgSrc;
-    img.width = 1000;
     // You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders
     // (Source code omitted for this binary file)
-
+    img.src = imgSrc;
+    img.width = 100;
     container.append(img);
+
+    const bgImg = document.createElement("div");
+    bgImg.className = "bgBox";
+    container.append(bgImg);
 
     return container;
 }
