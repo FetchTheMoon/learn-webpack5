7cc90e93f66a03e438b00e6ec6c71306a494e9cf
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 22:54:17 +0800

    每次要自己清理挺麻烦的, 不等教程了, 我搜索一番, 先装为敬

--- lg.config.js
+++ lg.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -70,5 +71,8 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins:[
+        new CleanWebpackPlugin(),
+    ]
 }
