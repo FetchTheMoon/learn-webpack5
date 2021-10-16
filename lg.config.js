f79c241c523715a81160d96c2353d1e51bd6999c
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 15:51:43 +0800

    loader的执行顺序是从右到左, 从下到上, 先执行的loader会将处理结果传递给下一个loader. 现在可以成功打包了. 样式也生效了

--- lg.config.js
+++ lg.config.js
@@ -11,7 +11,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/, // 用于匹配文件类型的正则表达式
-                use: ['css-loader', 'style-loader'], // CssSyntaxError
+                use: ['style-loader', 'css-loader'], // loader的执行顺序是从右到左, 从下到上, 先执行的loader会将处理结果传递给下一个loader
             }
         ]
     }
