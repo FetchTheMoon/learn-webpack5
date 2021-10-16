c099e3390d2fa5160baf497ca32d16f4eb642eb3
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 17:41:21 +0800

    直接require一个图片文件会报错

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import {sum, square} from './js/utils.js'; // 通过手动引入, 必须加js后缀, 否则报错找不到文件
-const getInfo = require('./js/api.js'); // Uncaught ReferenceError: require is not defined
-// import './js/lg'; // 将log.js加入依赖图(注意现在就没有加js后缀)
+// import {sum, square} from './js/utils.js'; // 通过手动引入, 必须加js后缀, 否则报错找不到文件
+// const getInfo = require('./js/api.js'); // Uncaught ReferenceError: require is not defined
+// // import './js/lg'; // 将log.js加入依赖图(注意现在就没有加js后缀)
+//
+// import './js/login';
+//
+// console.log(sum(10, 20));
+// console.log(square(10));
+// console.log(getInfo());
 
-import './js/login';
 
-console.log(sum(10, 20));
-console.log(square(10));
-console.log(getInfo());
+import './js/image';
---
+++ src/js/image.js
@@ -0,0 +1,17 @@
+function packImg() {
+
+    const container = document.createElement("div");
+
+    const img = document.createElement("img");
+
+    img.src = require('../img/0.jpg');
+    // You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders
+    // (Source code omitted for this binary file)
+
+    container.append(img);
+
+    return container;
+}
+
+
+document.body.append(packImg());
