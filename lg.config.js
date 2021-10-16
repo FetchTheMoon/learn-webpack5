efc6efee22628a518347a9ee8e53e39d7d84bfc6
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 16:49:32 +0800

    通过把对postcss的配置转移到`postcss.config.js`中(不要改名字), 可以提高复用性, 现在打包以后在浏览器debugger中也可以看到rgba被移动到最下面了(配置有效, 打包成功).

--- lg.config.js
+++ lg.config.js
@@ -14,18 +14,7 @@ module.exports = {
                 use: [
                     'style-loader',
                     'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    // require('autoprefixer')
-                                    'postcss-preset-env'
-                                ]
-                            }
-
-                        }
-                    }
+                    'postcss-loader'
                 ], // loader的执行顺序是从右到左, 从下到上, 先执行的loader会将处理结果传递给下一个loader
             },
             {
@@ -33,18 +22,7 @@ module.exports = {
                 use: [
                     'style-loader',
                     'css-loader',
-                    { // 假设处理less文件也需要用postcss做兼容性处理, 那么此时配置就显得有些冗余了(自信点, 非常冗余)
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    // require('autoprefixer')
-                                    'postcss-preset-env'
-                                ]
-                            }
-
-                        }
-                    },
+                    'postcss-loader',
                     'less-loader'],
             }
         ]
