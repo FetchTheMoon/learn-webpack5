23da557f1e3375aab27e85cadb528c1cfe71ffca
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-16 14:25:04 +0800

    使用node.js的path模块, 来给output.path指定绝对路径. 然后再用`npm run build`就可以了

---
+++ dist/build.js
@@ -0,0 +1,1 @@
+(()=>{var o={50:o=>{o.exports=()=>({name:"fet",age:22})}},e={};function r(t){var s=e[t];if(void 0!==s)return s.exports;var n=e[t]={exports:{}};return o[t](n,n.exports,r),n.exports}(()=>{"use strict";const o=r(50);console.log(30),console.log(10*10),console.log(o())})()})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-
+const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'build.js',
-        path: "./dist/main.js" // The provided value "./dist/main.js" is not an absolute path!
+        // path: "./dist/main.js" // The provided value "./dist/main.js" is not an absolute path!
+        path: path.resolve(__dirname, 'dist'),
     }
 }
