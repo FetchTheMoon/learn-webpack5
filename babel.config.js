ee623c43a54cd9d88175447c73298c076fe4e6f4
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-10-17 17:29:19 +0800

    在源码中添加包含`Promise`的语句, `useBuiltIns:'usage'` 根据源码用到什么,就填充什么

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     presets: [
-        '@babel/preset-env',
+        [
+            '@babel/preset-env',
+            {
+                //
+                useBuiltIns: 'usage', // 按需填充
+                corejs: 3 // 需要设置为3, 否则默认用的2, 会报错
+            }
+        ]
+
     ]
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ const foo = () => {
     console.log(title);
 }
 
+
+const p1 = new Promise((resolve, reject) => {
+    console.log(111);
+});
+
+console.log(p1);
+
+
 foo();
